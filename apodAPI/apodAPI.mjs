62fb7d130523b8926a21d9163a51679ad7df500e
Zachary Starbuck <zstarbuckgatech@gmail.com>
2025-01-18 16:33:57 -0500

    Revise Serverless Function

--- apodAPI/apodAPI.mjs
+++ apodAPI/apodAPI.mjs
@@ -1,13 +1,25 @@
 // Docs on request and context https://docs.netlify.com/functions/build/#code-your-function-2
-export default (request, context) => {
-  try {
-    const url = new URL(request.url)
-    const subject = url.searchParams.get('name') || 'World'
+// export default (request, context) => {
+//   try {
+//     const url = new URL(request.url)
+//     const subject = url.searchParams.get('name') || 'World'
 
-    return new Response(`Hello ${subject}`)
-  } catch (error) {
-    return new Response(error.toString(), {
-      status: 500,
-    })
+//     return new Response(`Hello ${subject}`)
+//   } catch (error) {
+//     return new Response(error.toString(), {
+//       status: 500,
+//     })
+//   }
+// }
+
+const handler = async(event) =>  {
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+    return {
+      statusCode: 200,
+      body: JSON.stringify({message: `Hello ${subject}`}),
+    }
+  } catch(error) {
+    return{statusCode: 500, body: error.toString()}
   }
 }
